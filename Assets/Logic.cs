ab044a067fad049e4d00240cf243084da27e0b7c
drklug <klugmaksim@gmail.com>
2018-01-26 23:36:01 +0800

    Project

---
+++ Assets/Logic.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
+
+public class Logic : MonoBehaviour {
+
+    public GameObject gameObject;
+    public VideoControl controller;
+    private UnityEngine.Video.VideoPlayer videoPlayer;
+    public GameObject sphere;
+    public GvrAudioSource sourse;
+    public GvrAudioSource sourse2;
+
+    public Text text1;
+    public Text text2;
+    public Button b1;
+    public Button b2;
+
+    private bool state1 = true;
+    private bool state2 = true;
+
+    private ColorBlock cb = new ColorBlock();
+
+    public string breakfastUrl = "https://www.dropbox.com/s/hghcb03wgoin7dx/breakfast.mp4?dl=1";
+    public string mountainUrl = "https://www.dropbox.com/s/om6v0p22d6y34b7/car_1.mp4?dl=1";
+    public string cliffUrl = "https://www.dropbox.com/s/cag0nlxuvhmobrx/car_2.mp4?dl=1";
+    public string carUrl = "https://www.dropbox.com/s/ifq30kdhmsbo0w0/car.mp4?dl=1";
+    public string stationUrl = "https://www.dropbox.com/s/lntare2lvm37hit/car_3.mp4?dl=1";
+
+    // Use this for initialization
+    void Start () {
+		videoPlayer = sphere.GetComponent<UnityEngine.Video.VideoPlayer>();
+        cb.normalColor = Color.white;
+        cb.pressedColor = Color.red;
+        b1.colors = cb;
+        b2.colors = cb;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+    public void ToggleUI()
+    {
+        videoPlayer.Play();
+        gameObject.transform.position = new Vector3(0f, 0f, 0f);
+        controller.isBegan = true;
+        if (state2)
+            sourse2.Play();
+    }
+    public void changeState1()
+    {
+        if (state1)
+        {
+            state1 = false;
+            text1.text = "Disabled";
+            sourse.Pause();
+        }
+        else
+        {
+            state1 = true;
+            text1.text = "Enabled";
+            sourse.Play();
+        }
+    }
+    public void changeState2()
+    {
+        if (state2)
+        {
+            state2 = false;
+            text2.text = "Disabled";
+            controller.isEnabled = false;
+        }
+        else
+        {
+            state2 = true;
+            text2.text = "Enabled";
+            controller.isEnabled = true;
+        }
+    }
+    public void PlayWhole()
+    {
+        videoPlayer.url = breakfastUrl;
+        controller.PlayingState = 1;
+        ToggleUI();
+    }
+    public void PlayCliff()
+    {
+        videoPlayer.url = cliffUrl;
+        controller.PlayingState = 0;
+        ToggleUI();
+    }
+    public void PlayCar()
+    {
+        videoPlayer.url = carUrl;
+        controller.PlayingState = 0;
+        ToggleUI();
+    }
+    public void PlayStation()
+    {
+        videoPlayer.url = stationUrl;
+        controller.PlayingState = 0;
+        ToggleUI();
+    }
+    public void PlayMountain()
+    {
+        videoPlayer.url = mountainUrl;
+        controller.PlayingState = 0;
+        ToggleUI();
+    }
+}
---
+++ Assets/VideoControl.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class VideoControl : MonoBehaviour
+{
+
+    private UnityEngine.Video.VideoPlayer videoPlayer;
+
+    public GameObject gameCam;
+    public bool isBegan = false;
+    private bool isPaused = false;
+    public bool isEnabled = true;
+    private Vector3 oldPlace = new Vector3(0.17f,2.79f,-15.27f);
+    public GvrAudioSource sourse;
+    public Logic lg;
+    public int PlayingState = 1;
+
+    void Start()
+    {
+        videoPlayer = GetComponent<UnityEngine.Video.VideoPlayer>();
+
+        if (videoPlayer.clip != null)
+        {
+            videoPlayer.EnableAudioTrack(0, true);
+        }
+    }
+
+    //Check if input keys have been pressed and call methods accordingly.
+    void Update()
+    {
+        //Play or pause the video.
+        if (Input.GetMouseButton(0) && isBegan == true)
+        {
+            if (videoPlayer.isPlaying)
+            {
+                videoPlayer.Pause();
+                sourse.Pause();
+                isPaused = true;
+            }
+            else
+            {
+                videoPlayer.Play();
+                if (isEnabled)
+                    sourse.Play();
+                isPaused = false;
+            }
+        }
+        if (!isPaused && !videoPlayer.isPlaying && isBegan)
+        {         
+            if (PlayingState == 0 || PlayingState == 5)
+            {
+                sourse.Pause();
+                isBegan = false;
+                gameCam.transform.position = oldPlace;
+            }
+            else if(PlayingState == 1)
+            {
+                videoPlayer.url = lg.carUrl;
+                videoPlayer.Play();
+                PlayingState = 2;
+            }
+            else if(PlayingState == 2)
+            {
+                videoPlayer.url = lg.mountainUrl;
+                videoPlayer.Play();
+                PlayingState = 3;
+            }
+            else if(PlayingState == 3)
+            {
+                videoPlayer.url = lg.cliffUrl;
+                videoPlayer.Play();
+                PlayingState = 4;
+            }
+            else if(PlayingState == 4)
+            {
+                videoPlayer.url = lg.stationUrl;
+                videoPlayer.Play();
+                PlayingState = 5;
+            }
+        }
+
+    }
+}
